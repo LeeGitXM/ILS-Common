ff67d5feec0147cdad6be06bed922637f3805e84
Chuck Coughlin <ccoughlin@ils-automation.com>
2015-04-26 12:14:16 -0600

    No need for GSON.

--- ILS_Common/src/com/ils/common/JavaToJson.java
+++ ILS_Common/src/com/ils/common/JavaToJson.java
@@ -3,54 +3,38 @@
  */
 package com.ils.common;
 
-import java.util.Hashtable;
-
-import com.google.gson.Gson;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inductiveautomation.ignition.common.util.LogUtil;
 import com.inductiveautomation.ignition.common.util.LoggerEx;
 
 /**
  *  JavaToJson is a class with static methods for converting
- *  HashTables and ArrayLists to JSON Strings.
+ *  HashTables and ArrayLists to JSON Strings. We use Jackson
+ *  as it handles generic Maps and Lists.
+ *  @see http://wiki.fasterxml.com/JacksonHome.
  */
 public class JavaToJson {
 	private static final String TAG = "JavaToJson";
 	private static LoggerEx log = LogUtil.getLogger(JavaToJson.class.getPackage().getName());
+	private static ObjectMapper mapper = new ObjectMapper();
 	
 	/**
-	 * Create a GSON equivalent of a single object.
+	 * Create a JSON string equivalent of a single object. The string
+	 * is a compressed (not pretty-print) version.
 	 * 
-	 * @param obj an object serializable to a Json string
-	 * @return the Json equivalent of the table that was supplied.
+	 * @param obj an object serializable to a Json string. It may be
+	 *            a POJO object, Map or List
+	 * @return the Json string equivalent of the object that was supplied.
 	 */
-	public synchronized String objectToJson(Object obj) {
-		Gson gson = new Gson();
-		String json="";
+	public synchronized String objectToJson(Object bean) {
+		String result = null;
 		try {
-			json = gson.toJson(obj);
+			result = mapper.writeValueAsString(bean);
 		}
-		catch(Exception ge) {
-			log.warnf("%s: objectToJson (%s)",TAG,ge.getMessage());
+		catch( JsonProcessingException jpe) {
+			log.warn(TAG+".opjectToJson: Parse exception ("+jpe.getLocalizedMessage()+")",jpe);
 		}
-		return json;
-	}	
-	
-	/**
-	 * This trivial. Given that the Java consists of Hashtables, arraylists and
-	 * primitives, simply use the Gson converter.
-	 * 
-	 * @param table a Hashtable serializable to a Json string
-	 * @return the Json equivalent of the table that was supplied.
-	 */
-	public synchronized String tableToJson(Hashtable<String,?> table) {
-		Gson gson = new Gson();
-		String json="";
-		try {
-			json = gson.toJson(table);
-		}
-		catch(Exception ge) {
-			log.warnf("%s: tableToJson (%s)",TAG,ge.getMessage());
-		}
-		return json;
+		return result; 
 	}	
 }
--- ILS_Common/src/com/ils/common/JsonToJava.java
+++ ILS_Common/src/com/ils/common/JsonToJava.java
@@ -3,6 +3,7 @@
  */
 package com.ils.common;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Iterator;
@@ -10,152 +11,73 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.inductiveautomation.ignition.common.util.LogUtil;
 import com.inductiveautomation.ignition.common.util.LoggerEx;
 
 /**
  *  JsonToJava is a class with  methods for converting a Json
- *  string into an object, Hashtable or ArrayList. We need to know
+ *  string into a bean object, HashMap or ArrayList. We need to know
  *  if the root object is a table,list or serializable object.
+ *  
+ *  We use Jackson as it handles generic Maps and Lists.
+ *  @see http://wiki.fasterxml.com/JacksonHome.
  */
 public class JsonToJava {
 	private static final String TAG = "JsonToJava";
 	private static LoggerEx log = LogUtil.getLogger(JsonToJava.class.getPackage().getName());
-
+	private static ObjectMapper mapper = new ObjectMapper();
+	
 	/**
-	 * Assuming the contents of the Dictionary represented by the Json string are Hashtables, 
-	 * ArrayLists, or simple objects, recursively convert the Json to a Java hashtable. 
-	 * If the input is null, return null.
+	 * We must know the expected class of the object being deserialized.
 	 * 
 	 * @param json the incoming Json document
 	 * @param clss the expected class of the object
 	 * @return the equivalent object
 	 */
 	public synchronized Object jsonToObject(String json,Class<?> clss) {
-		
-		Object result = null; 
-		if( json!=null && json.length()>0 )  {
-			Gson gson = new Gson();
-			try {
-				result = gson.fromJson(json, clss);
-			}
-			catch(Exception ex) {
-				log.error(TAG+"jsonToObject: deserialization exception ("+ex.toString()+")",ex);
-			}
+		Object result = null;
+		try {
+			result = mapper.readValue(json, clss);
+		} 
+		catch (JsonParseException jpe) {
+			log.warn(TAG+".jsonToObject: parsing exception ("+jpe.getLocalizedMessage()+")",jpe);
+		} 
+		catch (JsonMappingException jme) {
+			log.warn(TAG+".jsonToObject: mapping exception("+jme.getLocalizedMessage()+")",jme);
+		} 
+		catch (IOException ioe) {
+			log.warn(TAG+".jsonToObject: io exception("+ioe.getLocalizedMessage()+")",ioe);
 		}
 		return result;
 	}
 	
 	/**
-	 * Assuming the contents of the Dictionary represented by the Json string are Hashtables, 
-	 * ArrayLists, or simple objects, recursively convert the Json to a Java hashtable. 
-	 * If the input is null, return null.
+	 * Deserialize to a Map<String,String>. This is pretty restrictive.
 	 * 
 	 * @param json the incoming Json document
-	 * @return the equivalent hashtable
+	 * @param clss the expected class of the object
+	 * @return the equivalent object
 	 */
-	public synchronized Hashtable<String,?> jsonToTable(String json) {
-		Hashtable<String,?> result = null;
-		if( json!=null && json.length()>0 )  {
-			JsonParser parser = new JsonParser();
-			try {
-				JsonObject jobj = parser.parse(json).getAsJsonObject();
-				result = parseJsonDictionary(jobj);
-			}
-			catch(Exception ex) {
-				log.error(TAG+"jsonToTable: deserialization exception ("+ex.toString()+")",ex);
-			}
+	public synchronized Map<String,String> jsonToMap(String json) {
+		Map<String,String> result = null;
+		try {
+			result = mapper.readValue(json,new TypeReference<Map<String,String>>() { });
+		} 
+		catch (JsonParseException jpe) {
+			log.warn(TAG+".jsonToObject: parsing exception ("+jpe.getLocalizedMessage()+")",jpe);
+		} 
+		catch (JsonMappingException jme) {
+			log.warn(TAG+".jsonToObject: mapping exception("+jme.getLocalizedMessage()+")",jme);
+		} 
+		catch (IOException ioe) {
+			log.warn(TAG+".jsonToObject: io exception("+ioe.getLocalizedMessage()+")",ioe);
 		}
 		return result;
-	}
-	/**
-	 * Assuming the contents of the List represented by the Json string are Hashtables, 
-	 * ArrayLists, or simple objects, recursively convert the Json to a Java list. 
-	 * We require that all elements of the list be the same data type.
-	 * 
-	 * @param json the incoming Json document
-	 * @return the equivalent arraylist
-	 */
-	public synchronized List<?> jsonToList(String json) {
-		List<?> result = null;
-		if( json!=null && json.length()>0 )  {
-			JsonParser parser = new JsonParser();
-			try {
-				JsonArray array = parser.parse(json).getAsJsonArray();
-				result = parseJsonArray(array);
-			}
-			catch(Exception ex) {
-				log.error(TAG+": jsonToList: deserialization exception ("+ex.toString()+")",ex);
-			}
-		}
-		return result;
-	}
-
-	// Recognize dictionary elements as embedded dictionaries, lists or simple objects.
-	private Hashtable<String,?> parseJsonDictionary(JsonObject obj) {
-		Hashtable<String,Object> table = new Hashtable<String,Object>();
-		Set<Map.Entry<String, JsonElement>> set = obj.entrySet();
-		Iterator<Map.Entry<String, JsonElement>> iterator = set.iterator();
-		while (iterator.hasNext()) {
-			Map.Entry<String, JsonElement> entry = iterator.next();
-			String key = entry.getKey();
-			JsonElement value = entry.getValue();
-			if (!value.isJsonPrimitive()) {
-				if( value instanceof JsonObject ) {
-					Hashtable<String,?> subtable = parseJsonDictionary((JsonObject)value);
-					log.tracef("%s: parseJsonDictionary: DICTIONARY %s = %s",TAG,key,subtable);
-					table.put(key, subtable);
-				}
-				else if( value instanceof JsonArray ) {
-					List<?> list = parseJsonArray((JsonArray)value);
-					log.tracef("%s: parseJsonDictionary: ARRAY %s = %s",TAG,key,list.toString());
-					table.put(key, list);
-				}
-				else {
-					log.warnf("%s: parseJsonDictionary: not a JsonObject %s, class=%s",TAG,key,value.getClass().getName());
-				}
-			} 
-			else {
-				log.tracef("%s: parseJsonDictionary: %s=%s",TAG,key,value.getAsString());
-				table.put(key, value.getAsString());
-			}
-		}
-		return table;
-	}
-
-	// Recognize array elements as embedded dictionaries, lists or simple objects.
-	private List<?> parseJsonArray(JsonArray array) {
-		List<Object> list = new ArrayList<Object>();
-		int count = array.size();
-		int index = 0;
-		while( index < count ) {
-			JsonElement value = array.get(index);
-			if (!value.isJsonPrimitive()) {
-				if( value instanceof JsonObject ) {
-					Hashtable<String,?> subtable = parseJsonDictionary((JsonObject)value);
-					log.debug(TAG+"parseJsonArray: DICTIONARY "+subtable);
-					list.add(subtable);
-				}
-				else if( value instanceof JsonArray ) {
-					List<?> sublist = parseJsonArray((JsonArray)value);
-					log.debug(TAG+"parseJsonArray: ARRAY "+sublist);
-					list.add(sublist);
-				}
-				else {
-					log.debug(TAG+"parseJsonArray: not a JsonObject class="+value.getClass().getName());
-				}
-			} 
-			else {
-				log.debug(TAG+"parseJsonArray: "+value.getAsString());
-				list.add(value.getAsString());
-			}
-			index++;
-		}
-		return list;
 	}
 }
+
