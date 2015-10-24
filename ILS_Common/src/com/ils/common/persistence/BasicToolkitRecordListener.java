e615e6f3483c683fb2159803299da83c386e5734
Chuck Coughlin <ccoughlin@ils-automation.com>
2015-10-24 13:46:45 -0600

    Added Toolkit Record functions.

---
+++ ILS_Common/src/com/ils/common/persistence/BasicToolkitRecordListener.java
@@ -0,0 +1,42 @@
+/**
+ *   (c) 2015  ILS Automation. All rights reserved. 
+ */
+package com.ils.common.persistence;
+
+import com.inductiveautomation.ignition.common.util.LogUtil;
+import com.inductiveautomation.ignition.common.util.LoggerEx;
+import com.inductiveautomation.ignition.gateway.localdb.persistence.IRecordListener;
+import com.inductiveautomation.ignition.gateway.model.GatewayContext;
+import com.inductiveautomation.ignition.gateway.web.models.KeyValue;
+
+/**
+ * Respond to changes in our toolkit properties in the HSQL persistent database.
+ * For documentation relating to the SimpleORM data model:
+ * @See: http://simpleorm.org/sorm/whitepaper.html
+ */
+public class BasicToolkitRecordListener implements IRecordListener<ToolkitRecord> {
+	private final static String TAG = "ToolkitRecordListener";
+	protected final GatewayContext context;
+	protected final LoggerEx log;
+	
+	public BasicToolkitRecordListener(GatewayContext ctx) {
+		this.context = ctx;
+		this.log = LogUtil.getLogger(getClass().getPackage().getName());
+	}
+
+	@Override
+	public void recordAdded(ToolkitRecord rec) {
+		log.debugf("%s.recordAdded:",TAG);
+	}
+
+	@Override
+	public void recordDeleted(KeyValue rec) {
+		log.debugf("%s.recordDeleted:",TAG);
+	}
+
+	@Override
+	public void recordUpdated(ToolkitRecord rec) {
+		log.infof("%s.recordUpdated: %s = %s",TAG,rec.getName(),rec.getValue());	
+	}
+	
+}
---
+++ ILS_Common/src/com/ils/common/persistence/ToolkitRecordHandler.java
@@ -0,0 +1,69 @@
+/**
+ *   (c) 2014-2015  ILS Automation. All rights reserved.
+ *  
+ */
+package com.ils.common.persistence;
+
+import com.inductiveautomation.ignition.common.util.LogUtil;
+import com.inductiveautomation.ignition.common.util.LoggerEx;
+import com.inductiveautomation.ignition.gateway.model.GatewayContext;
+
+/**
+ *  This handler provides is a common interface for handling requests to save/restore 
+ *  properties in the ORM persistence engine.
+ *  
+ */
+public class ToolkitRecordHandler  {
+	private final static String TAG = "ToolkitRecordHandler";
+	private final LoggerEx log;
+	private GatewayContext context = null;
+    
+	/**
+	 * Initialize with instances of the classes to be controlled.
+	 */
+	public ToolkitRecordHandler(GatewayContext ctx) {
+		log = LogUtil.getLogger(getClass().getPackage().getName());
+		this.context = ctx;
+	}
+	
+
+	
+	/**
+	 * On a failure to find the property, an empty string is returned.
+	 */
+	public String getToolkitProperty(String propertyName) {
+		String value = "";
+		try {
+			ToolkitRecord record = context.getPersistenceInterface().find(ToolkitRecord.META, propertyName);
+			if( record!=null) value =  record.getValue();
+		}
+		catch(Exception ex) {
+			log.warnf("%s.getToolkitProperty: Exception retrieving %s (%s),",TAG,propertyName,ex.getMessage());
+		}
+		return value;
+	}
+	
+	/**
+	 * Set a name/value persistent property. Any update listeners will be notified.
+	 */
+	public void setToolkitProperty(String propertyName, String value) {
+		try {
+			ToolkitRecord record = context.getPersistenceInterface().find(ToolkitRecord.META, propertyName);
+			if( record==null) record = context.getPersistenceInterface().createNew(ToolkitRecord.META);
+			if( record!=null) {
+				record.setName(propertyName);
+				record.setValue(value);
+				context.getPersistenceInterface().save(record);
+				context.getPersistenceInterface().notifyRecordUpdated(record);
+			}
+			else {
+				log.warnf("%s.setToolkitProperty: %s=%s - failed to create persistence record (%s)",TAG,propertyName,value,ToolkitRecord.META.quoteName);
+			} 
+		}
+		catch(Exception ex) {
+			log.warnf("%s.setToolkitProperty: Exception setting %s=%s (%s),",TAG,propertyName,value,ex.getMessage());
+		}
+	}
+
+}
+
