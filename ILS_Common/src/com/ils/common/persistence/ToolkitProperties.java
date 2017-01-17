593d95a8afe114838c617020c29cef779c6c3220
chuckcoughlin <chuck_coughlin@comcast.net>
2017-01-17 15:19:28 -0700

    Added toolkit properties

--- ILS_Common/src/com/ils/common/persistence/ToolkitProperties.java
+++ ILS_Common/src/com/ils/common/persistence/ToolkitProperties.java
@@ -13,8 +13,12 @@ public interface ToolkitProperties   {
 	// These are the common names of toolkit properties that are to be stored in SQLite
 	// Note: Duplicates are intentional. The same strings are referenced in several places differently.
 	public static final String TOOLKIT_PROPERTY_DATABASE            = "Database";           // Production database
+	public static final String TOOLKIT_PROPERTY_BE_DATABASE         = "BatchExpertDatabase"; //Database for BatchExpert
+	public static final String TOOLKIT_PROPERTY_PYSFC_DATABASE      = "PySfcDatabase";       //Database for PySfc
+	public static final String TOOLKIT_PROPERTY_BE_DBMS             = "BatchExpertDBMS";     //Database for BatchExpert
+	public static final String TOOLKIT_PROPERTY_PYSFC_DBMS          = "PySfcDBMS";          //Database for PySfc
 	public static final String TOOLKIT_PROPERTY_ISOLATION_DATABASE  = "SecondaryDatabase";  // Database when in isolation
-	public static final String TOOLKIT_PROPERTY_ALTERNATE_DATABASE  = "SecondaryDatabase";  // Alternate database
+	public static final String TOOLKIT_PROPERTY_SECONDARY_DATABASE  = "SecondaryDatabase";  // Alternate database
 	public static final String TOOLKIT_PROPERTY_DBMS                = "DBMS";           // Production database DBMS
 	public static final String TOOLKIT_PROPERTY_ISOLATION_DBMS      = "SecondaryDBMS";      // Database DBMS when in isolation
 	public static final String TOOLKIT_PROPERTY_SECONDARY_DBMS      = "SecondaryDBMS";      // Alternate DBMS
